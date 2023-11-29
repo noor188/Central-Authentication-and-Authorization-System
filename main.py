8744ea855191a9b9d21ba3ffebe55469958ba9ef
Noor <noormmlk@gmail.com>
2023-11-28 23:57:37 -0700

    created 3 exception classes
    1. AuthException
    2. UsernameAlreadyExists
    3. PasswordTooShort

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import hashlib
 
+# methods
 def _encryptPw(self, password):
         '''Encrypt the password with the username and return the sha digest.'''
         hashString = (self.username + password)
         hashString = hashString.encode('UTF-8')
         return hashlib.sha256(hashString).hexdigest()
 
+# start exceptions
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        super().__init__(username, user)
+        self.username = username
+        self.user     = user
+
+class UsernameAlreadyExists(AuthException):
+    '''Exception to stop adding a user if that username already
+    exists in the dictionary. To prevent overwriting an existing user's data
+    and the new user might have access to that user's privileges.'''
+    pass
+
+class PasswordTooShort(AuthException):
+    '''Raise an exception if the password is too short.'''
+    pass
+
+# end exceptions 
 class User:
     '''Store the username and an encrypted password. Will allow a user to 
     login by checking whether an entered password is valid.'''
-    
+
     def __init__(self, username, password):
         '''Create a new user object. The password will be encrypted before storing.'''
-        self.username = username
-        self.password = _encryptPw(password)
+        self.username   = username
+        self.password   = _encryptPw(password)
         self.isLoggedIn = False
            
     def checkPassword(self, password):
