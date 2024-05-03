f57519f87897ce1bc19a0858d67accb4176a1a70
Noor <noormmlk@gmail.com>
2024-05-02 23:53:24 -0600

    created the authorizor class

--- main.py
+++ main.py
@@ -30,6 +30,20 @@ class InvalidUsername(AuthException):
 
 class InvalidPassword(AuthException):
     '''Raise an exception if the password does not match.'''
+    pass
+
+class PermissionError(Exception):
+    '''Raise an exception if permisiion either does not exist or
+    already exist'''
+    pass
+
+class NotLoggedInError(AuthException):
+    '''Raise an excpetion if the username is not logged in'''
+    pass
+
+class NotPermittedError(AuthException):
+    '''Raise an exception if the username is not grated the permission'''
+    pass
 
 # end exceptions 
 class User:
@@ -82,11 +96,55 @@ class Authenticator:
     def is_logged_in(self, username):
         '''Checks if a particular user is logged in'''
         if username in self.users:
-            return self.users[username].is_logged_in
+            return self.users[username].isLoggedIn
         return False
+
+
+class Authorizer:
+        ('''Deals woth permission and checs wherther a user can perform an activity'''
+        def __init__(self, authenticator):
+            self.authenticator = authenticator
+            self.permissions = {}
+        
+        def add_permission(self, perm_name):
+            '''Create a new permission that users can be added to'''
+            try:
+                perm_set = self.permissions[perm_name]
+            except KeyError:
+                self.permissions[perm_name] = set()                                
+            else:
+                raise PermissionError('Permission already exist')            
+
+        def permit_user(self, username, perm_name):
+            '''Grant the given permission to the user'''
+            try:
+                perm_set = self.permissions[perm_name]
+            except KeyError:
+                raise PermissionError('Permission does not exist')
+            else:
+                if username not in self.authenticator.users:
+                    raise InvalidUsername(username)
+                perm_set.add(username)
+
+        def check_permission(self, username, perm_name):
+            '''Checks whether the username is granted the given permission'''
+
+            if not self.authenticator.is_logged_in(username):
+                raise NotLoggedInError(username)
+
+            try:
+                perm_set = self.permissions[perm_name]
+            except KeyError:
+                raise PermissionError('Permission does not exist')
+            else:
+                if username in perm_set:
+                    return True
+                raise NotPermittedError(username))
     
 #instance
 authenticator =Authenticator()
+authorizer = Authorizer(authenticator)
+    
     
     
 
